8472223f89db5e67a1c68e63a72f52a4c6b83334
sparkleshine05 <mukti.bhargava05@gmail.com>
2026-09-20 19:02:36 +0530

    writing in a csv file using python

--- models.py
+++ models.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import csv
+with open("students.csv" , "w", newline="") as f: #f can be any name
+    writer = csv.writer(f)
+    writer.writerow(["Name","Age","Grade"])
+    writer.writerow(["Alice", 14, "8th"])
+    writer.writerow(["Bob", 15, "9th"])
+    writer.writerow(["Charlie", 14, "8th"])
+    writer.writerow(["Swati", 15, "9th"])
