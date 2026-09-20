4aedbef4a438278e3df1a2505d9df35e04c7a65b
sparkleshine05 <mukti.bhargava05@gmail.com>
2026-09-20 19:15:46 +0530

    json reading

--- json_loader.py
+++ json_loader.py
@@ -4,4 +4,9 @@ data = {"name": "Ashutosh", "score" : 92, "passed" : "True"}
 
 with open("data.json", "w") as f:
     json.dump(data, f)
-    
+
+
+with open("data.json", "r") as f:
+    loaded_data = json.load(f)
+    print(loaded_data)
+
